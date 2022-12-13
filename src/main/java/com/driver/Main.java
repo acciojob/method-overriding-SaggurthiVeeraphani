61e629d66cbe7eb8460da819224a1940b58710ce
Veera Phani <veeraphani21@gmail.com>
2022-12-13 09:07:07 +0530

    first push

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,17 @@
 package com.driver;
-
+class A{
+    public String meth(){
+        return "Invoking method from class A";
+    }
+}
+class B extends A{
+    @Override
+    public String meth() {
+        return super.meth();
+    }
+}
 public class Main {
-  
+    B obj = new B();
+    B.meth();
+
 }
